8cf3ed620cec50ac6dc3cdb828d61cca9bf40dc7
Zulham Syafrawi <official.zulham@gmail.com>
2022-01-05 15:31:14 +0700

    edit modal UI done

--- src/app/shopping/shopping-list-edit/shopping-list-edit.component.ts
+++ src/app/shopping/shopping-list-edit/shopping-list-edit.component.ts
@@ -1,15 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-shopping-list-edit',
+  selector: '[app-shopping-list-edit]',
   templateUrl: './shopping-list-edit.component.html',
-  styleUrls: ['./shopping-list-edit.component.css']
+  styleUrls: ['./shopping-list-edit.component.css'],
 })
 export class ShoppingListEditComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
