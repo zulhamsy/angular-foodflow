86bc146b5c794187f02626882731a94d60bfd12a
zulhamsy <zulham.sy@gmail.com>
2022-02-10 11:09:53 +0700

    change resep / menu from clicking card

--- src/app/recipe/recipe-list/recipe-list.component.ts
+++ src/app/recipe/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Recipe } from '../recipe.model';
 
 @Component({
@@ -7,18 +7,12 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css'],
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe(
-      'Nasi Goreng Jakarta',
-      'Nasi goreng spesial dari Jakarta',
-      'https://awsimages.detik.net.id/community/media/visual/2021/08/25/resep-nasi-goreng-sosis-ala-warung-bhakti_43.jpeg'
-    ),
-    new Recipe(
-      'Nasi Uduk',
-      'Nasi uduk yang kaga ada duanya',
-      'https://asset.kompas.com/crops/YJXX1aIeNs-2WHjw4VJDWJBywT0=/0x0:1000x667/750x500/data/photo/2020/07/05/5f01435b5c965.jpg'
-    ),
-  ];
+  @Input() resep: Recipe[];
+  @Output() clickResep = new EventEmitter();
+
+  selectResep($event: Recipe) {
+    this.clickResep.emit($event);
+  }
 
   constructor() {}
 
--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from './recipe.model';
 
 @Component({
   selector: '[app-recipe]',
@@ -6,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe.component.css'],
 })
 export class RecipeComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe(
+      'Nasi Goreng Jakarta',
+      'Nasi goreng spesial dari Jakarta',
+      'https://awsimages.detik.net.id/community/media/visual/2021/08/25/resep-nasi-goreng-sosis-ala-warung-bhakti_43.jpeg'
+    ),
+    new Recipe(
+      'Nasi Uduk',
+      'Nasi uduk yang kaga ada duanya',
+      'https://asset.kompas.com/crops/YJXX1aIeNs-2WHjw4VJDWJBywT0=/0x0:1000x667/750x500/data/photo/2020/07/05/5f01435b5c965.jpg'
+    ),
+  ];
+
+  currentRecipe: Recipe = new Recipe(
+    'Nasi Goreng Jakarta',
+    'Nasi goreng spesial dari Jakarta',
+    'https://awsimages.detik.net.id/community/media/visual/2021/08/25/resep-nasi-goreng-sosis-ala-warung-bhakti_43.jpeg'
+  );
+
+  changeResep($event: Recipe) {
+    this.currentRecipe = $event;
+  }
+
   constructor() {}
 
   ngOnInit(): void {}
