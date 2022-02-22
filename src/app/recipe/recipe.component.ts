7088e23077f0c0f7bf7c85d32810d3fba8f0e445
zulhamsy <zulham.sy@gmail.com>
2022-02-22 14:55:11 +0700

    add recipe services

--- src/app/recipe/recipe.component.ts
+++ src/app/recipe/recipe.component.ts
@@ -1,24 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { Recipe } from './recipe.model';
+import { RecipeService } from './recipe.service';
 
 @Component({
   selector: '[app-recipe]',
   templateUrl: './recipe.component.html',
   styleUrls: ['./recipe.component.css'],
+  providers: [RecipeService],
 })
 export class RecipeComponent implements OnInit {
-  recipes: Recipe[] = [
-    new Recipe(
-      'Nasi Goreng Jakarta',
-      'Nasi goreng spesial dari Jakarta',
-      'https://awsimages.detik.net.id/community/media/visual/2021/08/25/resep-nasi-goreng-sosis-ala-warung-bhakti_43.jpeg'
-    ),
-    new Recipe(
-      'Nasi Uduk',
-      'Nasi uduk yang kaga ada duanya',
-      'https://asset.kompas.com/crops/YJXX1aIeNs-2WHjw4VJDWJBywT0=/0x0:1000x667/750x500/data/photo/2020/07/05/5f01435b5c965.jpg'
-    ),
-  ];
+  recipes: Recipe[] = [];
 
   currentRecipe: Recipe = new Recipe(
     'Nasi Goreng Jakarta',
@@ -30,7 +21,9 @@ export class RecipeComponent implements OnInit {
     this.currentRecipe = resep;
   }
 
-  constructor() {}
+  constructor(private RecipeService: RecipeService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.recipes = this.RecipeService.getRecipes();
+  }
 }
---
+++ src/app/recipe/recipe.service.ts
@@ -0,0 +1,24 @@
+import { Injectable } from '@angular/core';
+import { Recipe } from './recipe.model';
+
+@Injectable({
+  providedIn: null,
+})
+export class RecipeService {
+  private recipes: Recipe[] = [
+    new Recipe(
+      'Nasi Goreng Jakarta',
+      'Nasi goreng spesial dari Jakarta',
+      'https://awsimages.detik.net.id/community/media/visual/2021/08/25/resep-nasi-goreng-sosis-ala-warung-bhakti_43.jpeg'
+    ),
+    new Recipe(
+      'Nasi Uduk',
+      'Nasi uduk yang kaga ada duanya',
+      'https://asset.kompas.com/crops/YJXX1aIeNs-2WHjw4VJDWJBywT0=/0x0:1000x667/750x500/data/photo/2020/07/05/5f01435b5c965.jpg'
+    ),
+  ];
+
+  getRecipes() {
+    return this.recipes.slice();
+  }
+}
