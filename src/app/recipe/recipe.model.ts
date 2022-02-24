08210ae226d0993ea35586a90b52252d595696df
zulhamsy <zulham.sy@gmail.com>
2022-02-24 11:35:17 +0700

    add bahan to recipe

--- src/app/recipe/recipe.model.ts
+++ src/app/recipe/recipe.model.ts
@@ -2,10 +2,12 @@ export class Recipe {
   public name: string;
   public description: string;
   public imagePath: string;
+  public recipe: string[];
 
-  constructor(name: string, desc: string, imagePath: string) {
+  constructor(name: string, desc: string, imagePath: string, recipe: string[]) {
     this.name = name;
     this.description = desc;
     this.imagePath = imagePath;
+    this.recipe = recipe;
   }
 }
--- src/app/recipe/recipe.service.ts
+++ src/app/recipe/recipe.service.ts
@@ -9,12 +9,14 @@ export class RecipeService {
     new Recipe(
       'Nasi Goreng Jakarta',
       'Nasi goreng spesial dari Jakarta',
-      'https://awsimages.detik.net.id/community/media/visual/2021/08/25/resep-nasi-goreng-sosis-ala-warung-bhakti_43.jpeg'
+      'https://awsimages.detik.net.id/community/media/visual/2021/08/25/resep-nasi-goreng-sosis-ala-warung-bhakti_43.jpeg',
+      ['Ayam Suwir', 'Nasi Putih', 'Bawang Merah', 'Bawang Putih']
     ),
     new Recipe(
       'Nasi Uduk',
       'Nasi uduk yang kaga ada duanya',
-      'https://asset.kompas.com/crops/YJXX1aIeNs-2WHjw4VJDWJBywT0=/0x0:1000x667/750x500/data/photo/2020/07/05/5f01435b5c965.jpg'
+      'https://asset.kompas.com/crops/YJXX1aIeNs-2WHjw4VJDWJBywT0=/0x0:1000x667/750x500/data/photo/2020/07/05/5f01435b5c965.jpg',
+      ['Nasi Putih', 'Santan Instan', 'Daun Bawang', 'Pandan']
     ),
   ];
 
